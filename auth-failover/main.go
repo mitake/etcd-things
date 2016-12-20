75090d437e327eec4b50b11cafa324f47a7ac41a
Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>
2016-12-20 16:09:05 +0900

    auth failover

---
+++ auth-failover/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/context"
+	"os"
+	"time"
+
+	"github.com/coreos/etcd/clientv3"
+)
+
+func main() {
+	c, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"http://127.0.0.1:2379", "http://127.0.0.1:22379", "http://127.0.0.1:32379"},
+		DialTimeout: 2 * time.Second,
+		Username:    "u1",
+		Password:    "p",
+	})
+
+	if err != nil {
+		fmt.Printf("failed to create a new client: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("client: %v\n", c)
+
+	kv := clientv3.NewKV(c)
+
+	for {
+		fmt.Printf("reading k1\n")
+
+		rsp, err := kv.Get(context.TODO(), "k1")
+		if err != nil {
+			fmt.Printf("Get failed: %s\n", err)
+		}
+
+		if len(rsp.Kvs) != 1 {
+			fmt.Printf("hmm, length of Kvs is %d\n", len(rsp.Kvs))
+		} else {
+			fmt.Printf("value of k1: %s\n", string(rsp.Kvs[0].Value))
+		}
+		<-time.After(1 * time.Second)
+	}
+}
